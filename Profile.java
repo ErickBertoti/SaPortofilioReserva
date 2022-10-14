6b5b627dc5472444a8fead740ca2a7d73bf054e2
Erick Hoffmann Bertoti <95359762+ErickBertoti@users.noreply.github.com>
2022-10-14 12:28:54 -0300

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,56 @@
+package com.example.saaplication;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.saaplication.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Profile extends AppCompatActivity {
+
+
+    private CircleImageView fotodeperfil;
+
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+
+    private Uri imageUri;
+    private String myUri = "";
+    private StorageTask uploadTask;
+    private StorageReference storageProfilePicsRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activit_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("User");
+        storageProfilePicsRef = FirebaseStorage.getInstance().getReference().child("Profile");
+
+        fotodeperfil = findViewById(R.id.fotodeperfil);
+
+
+
+
+            }
+        }
+
+
+
+
+
+
+
+
